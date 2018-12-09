a503481d25f540eaad1b6a1072e3cfd8e43286bd
sergiocalde94 <sergiocalde94@gmail.com>
2018-12-09 20:47:09 +0100

    Refactored frequency_fixer.py code
    - Added parse_frequencies function
    - New selection in part two, inverting the dictionary keys

--- day-one/src/frequency_fixer.py
+++ day-one/src/frequency_fixer.py
@@ -2,12 +2,17 @@ from collections import defaultdict
 import itertools as it
 
 
-def answer_function_part_one(filename: str) -> int:
+def parse_frequencies(filename: str) -> [int]:
     with open(filename) as frequency_changes:
         frequencies = map(lambda x: int(x[:-1]),
                           frequency_changes)
-        answer_first = sum(frequencies)
-    return answer_first
+        frequencies_list = list(frequencies)
+    return list(frequencies_list)
+
+
+def answer_function_part_one(filename: str) -> int:
+    frequencies = parse_frequencies(filename)
+    return sum(frequencies)
 
 
 def answer_function_part_two(filename: str) -> int:
@@ -15,17 +20,14 @@ def answer_function_part_two(filename: str) -> int:
         occurrences[frequency_change] += 1
         return occurrences[frequency_change] != 2
 
-    with open(filename) as frequency_changes:
-        occurrences = defaultdict(int)
-        frequencies = map(lambda x: int(x[:-1]),
-                          frequency_changes)
-        list(it.takewhile(lambda frequency: is_repeated(occurrences,
-                                                        frequency),
-                          it.accumulate(it.chain([0], it.cycle(frequencies)))))
-        answer_second = [key
-                         for key, value in occurrences.items()
-                         if value == 2][0]
-    return answer_second
+    occurrences = defaultdict(int)
+    frequencies = parse_frequencies(filename)
+
+    list(it.takewhile(lambda frequency: is_repeated(occurrences,
+                                                    frequency),
+                      it.accumulate(it.chain([0], it.cycle(frequencies)))))
+    occurrences_inverted = {value: key for key, value in occurrences.items()}
+    return occurrences_inverted[2]
 
 
 if __name__ == '__main__':
